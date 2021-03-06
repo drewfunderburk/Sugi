79eb8e6f4a245cec6421830d8bcfa20737ca2a94
Drew Funderburk <kevinfunderburkbusiness@gmail.com>
2021-03-06 17:58:01 -0600

    Added levels 1 2 and 3

--- Assets/_Game/Scripts/DeathPlane.cs
+++ Assets/_Game/Scripts/DeathPlane.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class DeathPlane : MonoBehaviour
@@ -7,5 +6,6 @@ public class DeathPlane : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(other.gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
